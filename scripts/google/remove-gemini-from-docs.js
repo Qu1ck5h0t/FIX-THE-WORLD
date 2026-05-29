925d65bc6249f610847aac41a3be566c4de96bec
Qu1ck5h0t <173572228+Qu1ck5h0t@users.noreply.github.com>
2026-05-29 01:03:34 -0400

    Update remove-gemini-from-docs.js

--- scripts/google/remove-gemini-from-docs.js
+++ scripts/google/remove-gemini-from-docs.js
@@ -1,1 +1,6 @@
+// ==UserScript==
+// @name         [Qu1ck5h0t's FIX THE WORLD] Remove Gemini from Google Docs
+// @match        https://docs.google.com/*
+// ==/UserScript==
 
+(()=>{d=document;s='.kixWizBarkickWrapper,#docs-sidekick-button-container,#docs-gemini-menu';f=_=>d.querySelectorAll(s).forEach(e=>e.remove());f();new MutationObserver(f).observe(d.documentElement,{childList:1,subtree:1})})();
